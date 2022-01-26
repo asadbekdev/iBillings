d20e357a8639cda8cbc7234875b441d38f41b3dc
asadbekdev <codementor7@gmail.com>
2022-01-26 16:48:01 +0500

    configure application files

---
+++ lib/resources/assets_manager.dart
@@ -0,0 +1,10 @@
+const String IMAGE_PATH = "assets/images";
+
+class ImageAssets {
+  static const String changeLangIc = "$IMAGE_PATH/change_lang_ic.svg";
+  static const String contactUsIc = "$IMAGE_PATH/contact_us_ic.svg";
+  static const String inviteFriendsIc = "$IMAGE_PATH/invite_friends_ic.svg";
+  static const String logoutIc = "$IMAGE_PATH/logout_ic.svg";
+}
+
+
---
+++ lib/resources/color_manager.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = HexColor.fromHex("#00A795");
+  static Color lightGreen = HexColor.fromHex("#00A795");
+  static Color darkGreen = HexColor.fromHex("#008F7F");
+  static Color dark = HexColor.fromHex("#2A2A2D");
+  static Color darker = HexColor.fromHex("#1E1E20");
+  static Color darkest = HexColor.fromHex("#141416");
+  static Color red = HexColor.fromHex("#FF426D");
+  static Color lightRed = HexColor.fromHex("#F85379");
+  static Color lightGrey1 = HexColor.fromHex("#F1F1F1");
+  static Color lightGrey = HexColor.fromHex("#F2F2F2");
+  static Color grey = HexColor.fromHex("#A6A6A6");
+  static Color darkGrey = HexColor.fromHex("#999999");
+  static Color white = HexColor.fromHex("#FFFFFF");
+}
+
+extension HexColor on Color {
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll('#', '');
+    if (hexColorString.length == 6) {
+      hexColorString = "FF" + hexColorString; // 8 char with opacity 100%
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
+}
